da303a6df0e252d55bfe763feb116a2eb538b3a7
Michelle Ross <miross629@gmail.com>
2022-11-07 14:43:02 -0800

    Display mushrooms with JS

--- app.js
+++ app.js
@@ -90,9 +90,12 @@ function displayFriends() {
 
 function displayMushrooms() {
     // clear out the mushroom div
+    mushroomsEl.textContent = '';
 
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
+        const mushroomEl = renderMushroom();
+        mushroomsEl.append(mushroomEl);
     }
 }
 
